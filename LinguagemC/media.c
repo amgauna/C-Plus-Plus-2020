f6c75417ca4de2f3883987a8490f697a6abebf7e
Ana Mercedes Gauna <amgauna@gmail.com>
2019-04-09 21:10:16 -0300

    Create media.c

---
+++ LinguagemC/media.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#include<conio.h>
+
+main()
+{
+   int contadoraluno=1, nota1=0, nota2=0, nota3=0, nota4=0, media=0; mediageral=0;
+   float aluno, nota1,nota2,nota3,nota4,media;
+   clrscr();
+   for contadoraluno <>0; contadoraluno < 24; contadoraluno++
+   {
+       printf ("Digite o valor da primeira nota:");
+       scanf ("%d",& nota1);
+       printf ("Digite o valor da segunda nota:");
+       scanf ("%d",& nota2);
+       printf ("Digite o valor da terceira nota:");
+       scanf ("%d",& nota3);
+       printf ("Digite o valor da quarta nota:");
+       scanf ("%d",& nota4);
+       media =(nota1+nota2+nota3+nota4)/4;
+       printf("A media do aluno " contadoraluno " foi %f", &media);
+       mediageral = mediageral + media;
+       if (media >=7) 
+          {                                                              
+            printf ("VOCE FOI APROVADO SUA MEDIA FINAL E %d", media);
+            getch();  contadoraluno                           
+          }
+       else
+          { 
+               printf ("VOCE FOI REPROVADO SUA MEDIA FINAL E %d", media);
+                getch();
+           }
+    }
+    printf("A media geral da turma eh: %.1f\n", mediageral/24);
+                                
+}
