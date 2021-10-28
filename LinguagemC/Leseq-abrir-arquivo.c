0871002fd0fa4c41ccb754540486a09ce89c5394
Ana Gauna <amgauna@gmail.com>
2021-10-27 23:42:08 -0300

    Update and rename Leseq.c to Leseq-abrir-arquivo.c

--- LinguagemC/Leseq-abrir-arquivo.c
+++ LinguagemC/Leseq-abrir-arquivo.c
@@ -5,8 +5,8 @@ main()
 	char x;
 	char name[50];
      double d;
-	if((ler = fopen("TESTE.TST","r")) == NULL) {
-		puts("Nao Posso Abrir TESTE.TST");
+	if((ler = fopen("TESTE.TXT","r")) == NULL) {
+		puts("Nao Posso Abrir TESTE.TXT");
 		exit();
 	}
 	fscanf(ler,"%c %d %s %lf", &x, &i, name, &d);
