cc0ac0dc71caa703883d187a40968d7f2cda016e
Ana Gauna <amgauna@gmail.com>
2021-11-30 22:02:58 -0300

    Create menu4.c

---
+++ menu4.c
@@ -0,0 +1,43 @@
+void menu ( void )
+      {
+          int opcao = 0;
+
+          printf ( “ 1 – inserir ” );
+          printf ( “ 2 – buscar ” );
+
+          scanf ( “%d”, &opcao );
+
+          switch ( opcao )
+                  {
+                      case 1: inserir ( a , b , c , d );
+                             break;
+
+                      case 5: exit();
+                 }
+
+       }
+
+ 
+
+ 
+
+int inserirlistaseq ( int lista[], int tam, int &quant, int valor )
+
+      {
+
+
+            menu()
+
+      }
+
+ 
+
+int main()
+
+   {
+
+      menu();
+
+      return (0);
+
+   }
