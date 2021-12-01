59138133f33adc5dc93ce870bd34a93863e2b2f5
Ana Gauna <amgauna@gmail.com>
2021-11-30 21:59:36 -0300

    Create menu-simples2.c

---
+++ menu-simples2.c
@@ -0,0 +1,22 @@
+int escolha = 0;
+
+do {
+    system("cls");
+ 
+    linha(80);
+    printf("\n\n\t\t\t>>> MENU PRINCIPAL <<<\n\n");
+    linha(80);
+    printf("\n\t 1 - C A D A S T R O S \n");
+    linha(80);
+    printf("\n\t 2 - C O M P R A S \n");
+    linha(80);
+    printf("\n\t 3 - V E N D A S \n");
+    linha(80);
+    printf("\n\t 4 - N O T A S  F I S C A I S  \n");
+    linha(80);
+    printf("\n\t 5 - S A I R  D O  P R O G R A M A\n");
+    linha(80);
+ 
+    escolha = getchar() % 48;
+
+} while (escolha < 1  || escolha > 5);
